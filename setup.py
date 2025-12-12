98e11e25aecdf76ba69c86b86545724544ff80a3
jr419 <jonathanrubin12@gmail.com>
2025-12-12 15:35:44 +0000

    Bump version from 0.1.0 to 1.0.0 in setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open("README.md", encoding="utf-8") as f:
 
 setup(
     name="bridge",
-    version="0.1.0",
+    version="1.0.0",
     description="BRIDGE: Block Rewiring from Inference-Derived Graph Ensembles",
     long_description=long_description,
     long_description_content_type="text/markdown",
